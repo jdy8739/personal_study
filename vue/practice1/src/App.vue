0b7df232f262f66b1ebfc7eac6ba6fd102e47971
jdy8739 <jdy8739@naver.com>
2021-12-31 00:35:27 +0900

    ..

--- vue/practice1/src/App.vue
+++ vue/practice1/src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="nav-bar">
+      <div>
+        <p>Ipsum Loren</p>
+        <p>about us</p>
+        <p>product</p>
+        <p>contact</p>
+      </div>
+    </div>
+
+    <h1 class="main-letter">{{ greetings }}</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  
+  },
+  data() {
+    return {
+      greetings: 'NEW START!'   
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+body {
+  margin: 0;
+}
+
+.nav-bar {
+  width: 100%;
+  height: 60px;
+  background-color: antiquewhite;
+}
+
+.nav-bar>div {
+  width: 100%;
+  max-width: 2000px;
+  display: flex;
+  margin: auto;
+}
+
+p {
+  padding: 0px 10px;
+  cursor: pointer;
+}
+
+.nav-bar p:first-child {
+  font-weight: bolder;
+  font-size: 18px;
+  flex-grow: 1;
+}
+
+.main-letter {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
