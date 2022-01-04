76c7882f78f0b90edcc91c4c5e36deb995e7be05
jdy8739 <jdy8739@naver.com>
2022-01-04 11:26:48 +0900

    ..

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -5,9 +5,11 @@ import './App.css';
 
 function App() {
 
-  const [ title, alter ] = useState( ['First Post', '01 . 03. 2022'] );
-  const [ title1, alter1 ] = useState( ['Second Post', '01 . 04. 2022'] );
-  const [ title2, alter2 ] = useState( ['Third Post', '01 . 06. 2022'] );
+  const posts = [['First Post', '01 . 03. 2022'], ['Where i live', '01 . 04. 2022'], ['Dinner party review', '01 . 06. 2022']];
+
+  const [ title, alter ] = useState(posts);
+  // const [ title1, alter1 ] = useState( ['Second Post', '01 . 04. 2022'] );
+  // const [ title2, alter2 ] = useState( ['Third Post', '01 . 06. 2022'] );
 
   let [ likedNum, likedNumAlter ] = useState(0);
 
@@ -15,6 +17,12 @@ function App() {
     const newArr = [...title]; //spread operator 필요
     newArr[0] = '여자코트 추천';
     alter(newArr); //원래 자료형에 맞게 넣어야한다. 변경 함수에 직접 넣어야 재렌더링이 원할하게 된다. 재렌더링하려면 useState로 변수를 만들고 전용 값 바꾸기 함수로 바꿔줘야한다.
+  }
+
+  function arrangeTitle() {
+    const sortedPosts = [...posts].sort();
+
+    alter(sortedPosts);
   }
 
   return (
@@ -28,21 +36,24 @@ function App() {
         <p>contact</p>
         <div style={ { width: '100px'} }></div>
       </div>
-      <button onClick={ changeTitle }>버튼</button>
       <div className='container'>
+        <div className='btn-section'>
+          <button onClick={ changeTitle }>버튼</button>
+          <button onClick={ arrangeTitle }>가나다 정렬</button>
+        </div>
         <div>
-          <h5 className='title'>{ title[0] }&ensp;<span onClick={ () => { likedNumAlter(likedNum + 1) } }>👍</span>&ensp;{ likedNum }</h5>
-          <p className='date'>{ title[1] }</p>
+          <h5 className='title'>{ title[0][0] }&ensp;<span onClick={ () => { likedNumAlter(likedNum + 1) } }>👍</span>&ensp;{ likedNum }</h5>
+          <p className='date'>{ title[0][1] }</p>
           <hr></hr>
         </div>
         <div>
-          <h5 className='title'>{ title1[0] }</h5>
-          <p className='date'>{ title1[1] }</p>
+          <h5 className='title'>{ title[1][0] }</h5>
+          <p className='date'>{ title[1][1] }</p>
           <hr></hr>
         </div>
         <div>
-          <h5 className='title'>{ title2[0] }</h5>
-          <p className='date'>{ title2[1] }</p>
+          <h5 className='title'>{ title[2][0] }</h5>
+          <p className='date'>{ title[2][1] }</p>
           <hr></hr>
         </div>
       </div>
