2b9af1d44068833914e4e1d0cc9c376cc91475fc
jdy8739 <jdy8739@naver.com>
2022-01-09 21:11:37 +0900

    ..

--- react2/shop/src/App.js
+++ react2/shop/src/App.js
@@ -6,14 +6,19 @@ import { Container, Row, Col, Button, Navbar, Nav, Card } from 'react-bootstrap'
 import importedProducts from './product';
 import { Link, Route, Switch } from 'react-router-dom';
 import { useHistory, useParams } from 'react-router-dom';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
 
 function App() {
   const red = { backgroundColor: 'red' };
   const pink = { backgroundColor: 'pink' };
   const green = { backgroundColor: 'green' };
 
+  const productStock = [30, 40, 25, 22, 44, 38, 29];
+
   const [products, altProducts] = useState(importedProducts);
+  const [stock, altStock] = useState(productStock);
 
   let history = useHistory();
 
@@ -35,6 +40,16 @@ function App() {
 
   function reset() {
     altProducts(importedProducts);
+  }
+
+  function showMoreProducts() {
+    axios.get('https://codingapple1.github.io/shop/data2.json')
+      .then((res) => {
+        altProducts([...products, ...res.data]);
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 
   return (
@@ -82,10 +97,11 @@ function App() {
                 })
               }
             </div>
+            <Button variant="primary" className='mt-5' onClick={ showMoreProducts }>상품 더보기</Button>
           </div>
         </Route>
         <Route exact path='/detail/:id'> { /* component={Detail}> */ }
-          <Detail products={products}/>
+          <Detail products={products} stock={stock} altStock={altStock}/>
         </Route>
       </div>
     </Switch>
@@ -110,14 +126,53 @@ function Product(props) {
 }
 
 
+let AlertBox = styled.div`
+  background-color: rgb(255, 0, 0, 0.15);
+  width: 100%;
+  height: 60px;
+  border-radius: 10px;
+  color: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
 function Detail(props) {
+
+  useEffect(() => {
+    const offAlertBox = setTimeout(() => {
+      alterisAlertShown(false);
+    }, 5000);
+
+    return () => { 
+      console.log('Component destroyed!');
+      clearTimeout(offAlertBox);
+    };
+  }, [  ]);
+
+  const [isAlertShown, alterisAlertShown] = useState(true);
 
   let history = useHistory();
   let { id } = useParams();
 
+
+  function subStock() {
+    const subedStock = [...props.stock];
+    subedStock[id] --;
+    props.altStock(subedStock);
+  }
+
   return (
     <>
       <div className="container">
+        { 
+          isAlertShown ?
+          <AlertBox className='mt-3'>
+            About to Sold Out!
+          </AlertBox>
+          : null
+        }
         <div className="row">
           <div className="col-md-6">
             <img src={ `https://codingapple1.github.io/shop/shoes${ parseInt(id) + 1 }.jpg` } width="100%" />
@@ -126,7 +181,7 @@ function Detail(props) {
             <h4 className="pt-5">{ props.products[id].title }</h4>
             <p>{ props.products[id].content }</p>
             <p>{ props.products[id].price }</p>
-            <button className="btn btn-danger">주문하기</button>
+            <button className="btn btn-danger" onClick={ subStock }>주문하기</button>
             &emsp;
             <button className="btn btn-danger" onClick={ () => { history.goBack(); } }>
               뒤로
@@ -134,10 +189,20 @@ function Detail(props) {
             &emsp;
             <button className="btn btn-danger" onClick={ () => { history.push('/products'); } }>
               모든 상품 보기
-            </button> 
+            </button>
+            <Stock stock={props.stock[id]}/>
           </div>
         </div>
       </div> 
+    </>
+  )
+}
+
+
+function Stock(props) {
+  return (
+    <>
+      <p className='mt-5'>남은 재고 { props.stock }개</p>
     </>
   )
 }
