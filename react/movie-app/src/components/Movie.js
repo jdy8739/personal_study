a17e2725b5659ad0299b0bb41b17eb19e1307214
jdy8739 <jdy8739@naver.com>
2022-01-12 18:36:48 +0900

    ,,

--- react/movie-app/src/components/Movie.js
+++ react/movie-app/src/components/Movie.js
@@ -10,18 +10,18 @@ function Movie({ movieImg, title, year, summary, genres, bg, id }) {
                     <div>
                         <h3><Link to={ `/detail/${ id }` }>{ title } { year }</Link></h3>
                         {/* a 태그는 새로고침되므로 Link to 가 좋음 */}
-                        <p>{ summary }</p>
+                        <p>{ summary.length > 450 ? `${summary.slice(0, 450)}...` : summary }</p>
+                        <ul>
+                        {
+                            genres.map((genre, i) => {
+                                return (
+                                    <li key={i}>{ genre }</li>
+                                )
+                            })
+                        }
+                        </ul>
                     </div>
                 </div>
-                <ul>
-                {
-                    genres.map((genre, i) => {
-                        return (
-                            <li key={i}>{ genre }</li>
-                        )
-                    })
-                }
-                </ul>
                 <div className='movie-bg-img' style={{ backgroundImage: `url(${bg})` }}></div>
             </div>
         </>
