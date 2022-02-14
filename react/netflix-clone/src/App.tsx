e51ae0ced93134cf301da054b010d0ab69d84572
jdy8739 <jdy8739@naver.com>
2022-02-14 23:54:42 +0900

    ..

--- react/netflix-clone/src/App.tsx
+++ react/netflix-clone/src/App.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Search from './components/Search';
+import Tv from './components/Tv';
+import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/search' element={ <Search /> } />
+          <Route path='/tv' element={ <Tv /> } />
+          <Route path='/' element={ <Home /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
