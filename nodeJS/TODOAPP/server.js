509f6444a0d59dc9445a3d8afcf68c5df4e1bca4
jdy8739 <jdy8739@naver.com>
2022-02-22 22:40:38 +0900

    ..

--- nodeJS/TODOAPP/server.js
+++ nodeJS/TODOAPP/server.js
@@ -2,9 +2,18 @@ const express = require('express');
 const app = express();
 const bodyparser = require('body-parser');
 app.use(bodyparser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
 
-app.listen(8080, () => {
-    console.log('listening on 8080.');
+const MongoClient = require('mongodb').MongoClient;
+const URL = 'mongodb+srv://jdy8739:AzC0ynC4Kkl30Imt@cluster0.kz1h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+let db;
+
+MongoClient.connect(URL, function(error, client){
+    if(error) return console.log(error);
+    app.listen('8080', function() {
+        db = client.db('todoapp');
+        console.log('listening on 8080');
+    });
 });
 
 app.get('/', (req, res) => {
@@ -27,5 +36,19 @@ app.post('/add', (req, res) => {
     console.log(req.body.title);
     console.log(req.body.content);
     console.log(req.body);
+
+    const newTodo = { title: req.body.title, content: req.body.content, date: Date.now() };
+    db.collection('post').insertOne(newTodo, function() {
+        console.log('new todo saved.');
+    });
+
     res.send('complete.');
+});
+
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray(function(err, data) {
+        console.log(data);
+        res.render('list.ejs', { data });
+    });
 });
